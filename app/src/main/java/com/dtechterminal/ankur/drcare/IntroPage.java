b8744c16dd7d351ec0d91e89be9cbdb5e94375e3
Ankur Pandey <ankurpandeyvns@gmail.com>
2017-11-18 14:25:14 +0530

    Initial Upload

---
+++ app/src/main/java/com/dtechterminal/ankur/drcare/IntroPage.java
@@ -0,0 +1,13 @@
+package com.dtechterminal.ankur.drcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_page);
+    }
+}
